3adcbfc3cf1a92a2af50009375db2de497f1442c
guiiids <66186056+guiiids@users.noreply.github.com>
2024-05-19 09:32:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,15 @@
 import os
 import streamlit as st
 from openai import OpenAI
-from dotenv import load_dotenv
 import openai
 
 
 # Load environment variables from .env file
-load_dotenv()
 
 # Get the API key from the environment variable
-api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key="sk-proj-FBMfNzQ9tgPTVEBLMSrXT3BlbkFJHR12R0vKrUknUaGsqvh0")
 
-if not api_key:
-    raise ValueError("No API key found. Please set the OPENAI_API_KEY environment variable.")
 
-# Initialize the OpenAI client with the API key
-client = OpenAI(api_key=api_key)
 
 
 def main():
@@ -36,9 +30,12 @@ def main():
                             You are a tech support assistant specializing in CrossLab Connect (CLC). Provide clear, concise, step-by-step instructions to answer users' technical questions. Ensure responses do not exceed 600 tokens. Aim for brevity and clarity. 
 
                             1. Always present steps in a numbered list (e.g., 1, 2, 3).
-                            2. Do not use headlines for each step.
-                            3. Use text formatting such as **bold**, *italic*, and __underline__ to highlight UI elements, buttons, links, pages, or sections (e.g., **Service History** button or **Menu** icon).
-                            4. Do not answer questions outside the scope of CrossLab Connect.
+                            2. Double check your knowledge before answering the question. Cross check your reasoning versus the knowledge you have, to see if your answer is accurate and detailed.
+                            3. Always output hyperlinks when possible. 
+                            4. Do not use headlines for each step.
+                            5. Use text formatting such as **bold**, *italic*, and __underline__ to highlight UI elements, buttons, links, pages, or sections (e.g., **Service History** button or **Menu** icon).
+                            6. Do not answer questions outside the scope of CrossLab Connect.
+
                             """
                         },
                         {
@@ -50,15 +47,15 @@ def main():
                     top_p=1,
                     frequency_penalty=0,
                     presence_penalty=0
-                )
+                ) 
+                response_content = response.choices[0].message.content
 
-                # Process response and display results
-                st.write(response.choices[0].message['content'])
+                # Print the entire response for debugging purposes
+                # st.write("Debug Info: Full Response")
+                st.write(response_content)
 
-
-            except openai.APIError as e:
+                
+            except openai.error.OpenAIError as e:
                 st.error(f"An error occurred: {e}")
-
-
 if __name__ == "__main__":
     main()
