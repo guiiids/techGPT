1e23263c7ea48da4ff348356e4f7616777f90485
guiiids <66186056+guiiids@users.noreply.github.com>
2024-05-20 13:28:34 -0400

    Create speech_to_text.py

---
+++ pages/speech_to_text.py
@@ -0,0 +1,31 @@
+import os
+import streamlit as st
+from openai import OpenAI
+import openai
+
+# Get the API key from the environment variable
+
+
+my_api_key = st.secrets["api_keys"]["OPENAI_API_KEY"]
+
+client = OpenAI(api_key=my_api_key)
+
+# Path to the audio file
+audio_file_path = 'docs/audio/smart_alerts.mp3'
+
+# Open the audio file
+with open(audio_file_path, "rb") as audio_file:
+    # Transcribe the audio using OpenAI's Whisper model
+    transcription = openai.Audio.transcribe(
+        model="whisper-1",
+        file=audio_file
+    )
+
+    # Extract the transcribed text
+    transcribed_text = transcription['text']
+
+    # Save the transcription to a text file
+    with open('transcription.txt', 'w') as file:
+        file.write(transcribed_text)
+
+print("Transcription saved to transcription.txt")
